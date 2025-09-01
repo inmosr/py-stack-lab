3b5d970f9db894a3826a5956b834c13ce174fa8c
inmosr <inmo.sr19@gmail.com>
2025-09-01 17:11:21 -0600

    Update caesar_encrypt.py
    Finishing project

--- caesar_encrypt.py
+++ caesar_encrypt.py
@@ -1,19 +1,38 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+import art
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+print(art.logo)
 
 
-# TODO-1: Create a function called 'encrypt()' that takes 'original_text' and 'shift_amount' as 2 inputs.
 
-def encrypt(original_text,shift_amount):
-    
-# TODO-2: Inside the 'encrypt()' function, shift each letter of the 'original_text' forwards in the alphabet
-#  by the shift amount and print the encrypted text.
+def caesar(direction,original_text,shift_amount):
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u','v', 'w', 'x', 'y', 'z']
+    if direction == 'decode':
+        shift_amount *= -1
+    output = ''
+    for letter in original_text:
+        if letter in alphabet:
+            output += alphabet[(alphabet.index(letter) + shift_amount) % len(alphabet)]
+        else:
+            output += letter
+    return output
 
-# TODO-4: What happens if you try to shift z forwards by 9? Can you fix the code?
 
-# TODO-3: Call the 'encrypt()' function and pass in the user inputs. You should be able to test the code and encrypt a
-#  message.
+while 1:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    print(caesar(direction,text,shift))
+    exit = ''
+    while 1:
+        exit = input('Would you like to encode/decode a text again?y/n ')
+        if exit == 'y':
+            break
+        elif exit == 'n':
+            break
+        else:
+            print('Invalid option')
+    if exit == 'n':
+        break
+
+
 
